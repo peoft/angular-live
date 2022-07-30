3e5977ecdffa034a44ad6bda07c4d1fd81b56c72
peoft <paulo.ostermann.filho@gmail.com>
2022-07-30 15:34:05 -0300

    Diretiva ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-firs-project';
+export class AppComponent implements OnInit{
+  count: number  = 0;
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    let interval = setInterval(() => {
+      this.count++;
+      if (this.count == 10) {
+        clearInterval(interval);
+      }
+    }, 1000)
+  }
 }
