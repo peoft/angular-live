5221354a47ad954cf34c8ff9afc40a2459e66071
peoft <paulo.ostermann.filho@gmail.com>
2022-07-31 08:39:48 -0300

    Adicionando uma interface para people array

--- src/app/shared/services/people.service.ts
+++ src/app/shared/services/people.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
+interface IPessoa { firstName: string, lastName: string, age: number };
+
 @Injectable({
     
   providedIn: 'root'
 })
+
 export class PeopleService {
 
   constructor() { }
 
   getPeople():Observable<any> {
-    let peopleArray = [
+    let peopleArray:IPessoa [];
+
+    peopleArray = [
       {
         firstName: 'Ivonaldo',
         lastName: 'Soares',
